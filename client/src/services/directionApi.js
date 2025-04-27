c593d3d76737054cc16042f9b9beee976b0fa156
letabasco <brax2408@gmail.com>
2025-04-27 23:37:18 +0900

    현재까지의 변경 사항

--- client/src/services/directionApi.js
+++ client/src/services/directionApi.js
@@ -1,6 +1,7 @@
 // src/services/directionApi.js
 export const directionRequest = async (start, goal) => {
-  const url = `http://localhost:3001/direction?start=${start}&goal=${goal}`;
+  const PROXY_URL = 'http://15.164.94.96:3001';
+  const url = `${PROXY_URL}/direction?start=${start}&goal=${goal}`;
   const response = await fetch(url, {
     method: "GET",
   });
